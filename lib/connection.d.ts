9ab894afdf2a38219c8da7cc29d33bc48f22347f
Pierre Voisin <pierre.voisin@montreal.ca>
2022-05-09 14:48:00 -0400

    Enhance TypeScript declarations (`Connection`)

--- lib/connection.d.ts
+++ lib/connection.d.ts
@@ -1,5 +1,6 @@
 import { Readable } from 'stream';
 import { Records } from './records';
+import { ResultSet } from './resultset';
 
 declare enum QueryTypes {
   prefix, // PQF (Prefix Query Format)
@@ -12,17 +13,46 @@ declare enum QueryTypes {
 export type QueryType = keyof typeof QueryTypes;
 
 export class Connection extends Readable {
+  /**
+   * Creates a connection to the specified host.
+   *
+   * Note: user & password can be set using `#set` method like:
+   * ```
+   * connection
+   *   .set('user', 'johnny')
+   *   .set('password', 'ca$h')
+   * ```
+   *
+   * @param host Host to connect to, like "z3950.loc.gov:7090".
+   */
+  constructor(host: string);
+
   /** Sets specified connection property. */
   set(key: string, val: string): this;
 
   /** Returns specified connection property. */
   get(key: string): string;
 
-  /** Performs specified query. */
+  /**
+   * Prepares search query using specified query type.
+   *
+   * Note: returned value is actually a clone of the connection.
+   */
   query(type: QueryType, queryString: string): this;
 
-  /** Performs specified query. */
+  /**
+   * Prepares search query.
+   *
+   * Note: returned value is actually a clone of the connection.
+   */
   query(queryString: string): this;
+
+  /**
+   * Searches for records using previously specified query.
+   *
+   * @see #query
+   */
+  search(cb: (error: Error, results: ResultSet) => void): this;
 
   /** Updates specified record. */
   updateRecord(
